afcf9405dcbad0aff4573a0e35d75b9328ac0b00
ladrianusc <ladrian@ullimanschutte.com>
2017-04-25 11:24:40 -0400

    added counter, fixed budget total, changed saftey

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var countFormat = new Intl.NumberFormat('en-us', {minimumIntegerDigits: 3});
+	var last = new Date(2017, 1, 1);
+	var now = new Date();
+	var utcLast = Date.UTC(last.getFullYear(), last.getMonth(), last.getDate());
+	var utcNow = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+	var timeDiff = Math.abs(utcNow - utcLast);
+	var count = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+	$(".counter").text(countFormat.format(count));
+	
+});
