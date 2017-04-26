84f2033a32b993e9660d1c7de9380f0ade8ecdd3
ladrianusc <ladrian@ullimanschutte.com>
2017-04-26 15:36:50 -0400

    fixed counter date 4 to 3 because 0123 months

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var countFormat = new Intl.NumberFormat('en-us', {minimumIntegerDigits: 3});
-	var last = new Date(2017, 4, 3);
+	var last = new Date(2017, 3, 3);
 	var now = new Date();
 	var utcLast = Date.UTC(last.getFullYear(), last.getMonth(), last.getDate());
 	var utcNow = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
